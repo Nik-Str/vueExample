06c1598ccda3df98dff35c099e3e4f1e0ec11540
Nik-Str <89907988+Nik-Str@users.noreply.github.com>
2022-05-12 13:46:18 +0200

    home video added

--- vue-example3/src/components/Navbar.vue
+++ vue-example3/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-transparent border-bottom">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand">
         <img src="../assets/logo.png" alt="" :style="{ width: '10rem', height: 'auto' }"
--- vue-example3/src/views/HomeView.vue
+++ vue-example3/src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="videoContainer">
+      <video autoplay muted loop class="myVideo">
+        <source src="../assets/home.mp4" type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
+    </div>
+    <div class="content">
+      <h1>WELCOME TO IUDC</h1>
+      <h4><i>Styles from all around the world!</i></h4>
+    </div>
   </div>
 </template>
 
@@ -9,3 +18,43 @@ export default {
   name: 'HomeView',
 };
 </script>
+
+<style lang="scss" scoped>
+.videoContainer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  overflow: hidden;
+}
+.videoContainer > video {
+  display: block;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+}
+@media screen and (max-aspect-ratio: 1920/1080) {
+  .videoContainer > video {
+    height: 100%;
+  }
+}
+@media screen and (min-aspect-ratio: 1920/1080) {
+  .videoContainer > video {
+    width: 100%;
+  }
+}
+
+.content {
+  position: absolute;
+  top: 40vh;
+  width: 100vw;
+  text-align: center;
+  h1 {
+    font-weight: bolder;
+  }
+}
+</style>
