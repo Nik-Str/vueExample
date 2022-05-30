29741f9f96cd07c3ba094de8a9645e2077e57631
Nik-Str <89907988+Nik-Str@users.noreply.github.com>
2022-05-30 16:13:59 +0200

    comments added

--- vue-example3/src/store/index.js
+++ vue-example3/src/store/index.js
@@ -40,6 +40,7 @@ export default createStore({
       state.isError = null;
     },
     setData(state, payload) {
+      //Init state after data fetch
       state.data = payload;
       state.isLoading = false;
       state.isError = null;
@@ -47,6 +48,7 @@ export default createStore({
       let categories = [];
       let colors = [];
       let brands = [];
+      //Get values for filter mechanism
       for (let i = 0; i < state.data.length; i++) {
         if (!categories.some((item) => item === state.data[i].category)) {
           categories.push(state.data[i].category);
@@ -58,6 +60,7 @@ export default createStore({
           brands.push(state.data[i].brand);
         }
       }
+      //Init state for filters
       state.allCategories = categories;
       state.allColors = colors;
       state.allBrands = brands;
@@ -139,11 +142,13 @@ export default createStore({
       localStorage.removeItem('favourites');
     },
     addToCart(state) {
+      //Controll if item added already exist in cart
       if (
         state.cart.some(
           (item) => item.article_nr === state.modal.article_nr && item.selectedSize === state.modalSelectedSize
         )
       ) {
+        // Increment existing item in cart
         state.cart = state.cart.map((item) => {
           if (item.article_nr === state.modal.article_nr && item.selectedSize === state.modalSelectedSize) {
             return { ...item, total: item.total + state.modal.price, amount: item.amount + 1 };
@@ -151,12 +156,14 @@ export default createStore({
             return item;
           }
         });
+        //Add new item in cart
       } else {
         state.cart = [
           ...state.cart,
           { ...state.modal, total: state.modal.price, amount: 1, selectedSize: state.modalSelectedSize },
         ];
       }
+      // Update semipersistent state for cart in localstorage
       localStorage.setItem(
         'cart',
         JSON.stringify(
@@ -216,10 +223,16 @@ export default createStore({
     },
     initSupportChat(state, payload) {
       if (!state.webSocket) {
+        //Init new websocket in state
         state.webSocket = new WebSocket(`ws://localhost:3000/${payload}`);
+
+        //On new message
         state.webSocket.addEventListener('message', (response) => {
           const support = JSON.parse(response.data);
+          //Display new msg to client
           if (!state.displaySupport) state.clientHasNewMsg = true;
+
+          //Init client and support websocket states
           if (support.ticketId && !state.supportTicketId) {
             state.supportTicketId = support.ticketId;
           } else if (support.supportId && state.supportTicketId) {
@@ -234,7 +247,10 @@ export default createStore({
             state.supportChat = [...state.supportChat, support];
           }
         });
+
+        //On support-ticket close
         state.webSocket.addEventListener('close', () => {
+          //Reinit websocket and ticket states
           state.webSocket.close();
           state.webSocket = null;
           state.supportChat = [];
@@ -294,13 +310,16 @@ export default createStore({
       }
     },
     getCart(state) {
+      //Check for previous added cart items in semipersistent storage
       const storage = JSON.parse(localStorage.getItem('cart'));
       if (storage) {
+        //Get cart items data from server
         const articles = storage.map((item) => item.article);
         axios
           .post(`${BASE_URL}/products`, { products: articles })
           .then((res) => {
             let cart = [];
+            //Reinit previous cart state
             for (let i = 0; i < storage.length; i++) {
               const item = res.data.filter((item) => item.article_nr === storage[i].article);
               cart.push({
@@ -310,6 +329,7 @@ export default createStore({
                 total: storage[i].amount * item[0].price,
               });
             }
+            //Set cart state
             state.commit('setInitialCart', cart);
           })
           .catch((err) => state.commit('setError', err.message));
@@ -358,6 +378,7 @@ export default createStore({
         items = items.filter((item) => state.selectedBrand.includes(item.brand));
       }
 
+      //Filter by size
       if (state.selectedSize.length >= 1) {
         items = items.filter((item) => {
           if (item.s) {
